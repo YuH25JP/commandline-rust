b743b215735af4a4f5c1cb6f0d7fe1d6e22cc95c
YuH25JP <cppyuh@gmail.com>
2024-02-06 13:36:37 +0900

    completed uniqr

--- uniqr/src/lib.rs
+++ uniqr/src/lib.rs
@@ -1,9 +1,8 @@
 use clap::Parser;
 use std::{
-    collections::HashMap,
     error::Error,
     fs::File,
-    io::{self, BufRead, BufReader},
+    io::{self, BufRead, BufReader, Write},
 };
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
@@ -36,25 +35,46 @@ fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
     }
 }
 
+fn format_string(flag: &bool, pair: &(i32, String)) -> String {
+    match flag {
+        true => format!("{:>4} {}", pair.0, pair.1),
+        false => format!("{}", pair.1),
+    }
+}
+
 pub fn run(args: Args) -> MyResult<()> {
     let mut file = open(&args.in_file).map_err(|e| format!("{}: {}", args.in_file, e))?;
+    let mut out_file: Box<dyn Write> = match &args.out_file {
+        Some(out_file_name) => Box::new(File::create(out_file_name)?),
+        _ => Box::new(io::stdout()),
+    };
     let mut line = String::new();
-    let mut hm = HashMap::new();
+    let mut pair = (0, "".to_string());
+    let mut first = true;
+
     loop {
-        let mut bytes = file.read_line(&mut line)?;
+        let bytes = file.read_line(&mut line)?;
         if bytes == 0 { break; }
 
-        let count = hm.entry(line.clone()).or_insert(0);
-        *count += 1;
+        if first {
+            pair.0 = 1;
+            pair.1 = line.clone();
+            first = false;
+        } else {
+            if pair.1.trim_end() == line.trim_end() {
+                pair.0 += 1;
+            } else {
+                write!(out_file, "{}", format_string(&args.count, &pair))?;
+                
+                pair.0 = 1;
+                pair.1 = line.clone();
+            }
+        }
 
         line.clear();
     }
-    for (k, v) in hm.into_iter() {
-        if args.count {
-            print!("{:>4} {}", v, k);
-        } else {
-            print!("{}", k);
-        }
+    if pair.0 != 0 { // when the input file is empty, this line is skipped
+        write!(out_file, "{}", format_string(&args.count, &pair))?;
     }
     Ok(())
 }
